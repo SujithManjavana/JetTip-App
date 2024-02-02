c5086d7f45d67b0d0276dda738a3b8390d1654c0
sujithmanjavana <sujithmanjavana@gmail.com>
2024-02-02 13:25:23 +0530

    add top header

--- app/src/main/java/com/sujith/jettipapp/MainActivity.kt
+++ app/src/main/java/com/sujith/jettipapp/MainActivity.kt
@@ -4,43 +4,62 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sujith.jettipapp.ui.theme.JetTipAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetTipAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp {
+                Text("Foo")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+fun MyApp(content: @Composable () -> Unit) {
     JetTipAppTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            content()
+        }
     }
 }
+
+@Preview
+@Composable
+fun TopHeader() {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .height(150.dp).clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color.DarkGray
+        ) {
+
+    }
+}
+
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MyApp {
+//        TopHeader()
+//    }
+//}
